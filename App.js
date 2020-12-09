a24e1153a3fe6c21712bb51e989cc766c63b507a
alestor123 <alestoraldous@gmail.com>
2020-12-09 17:26:46 +0530

    added cmnt

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
 module.exports = (json) => {
-return JSON.stringify(json).replace(/{|}|"/g,'').replace(/:|"/g,'=').replace(/,/g,'\n')
+return JSON.stringify(json).replace(/{|}|"/g,'').replace(/:|"/g,'=').replace(/,/g,'\n') //please dont use this method it was made in a hurry use the proper method. will be update soon
 }
